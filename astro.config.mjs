c5f102bced63040beaac21a341df3f42dd9396c6
Gawaksh <gawaksh@Gawakshs-MacBook-Pro.local>
2025-01-27 15:52:55 +0530

    Configuring Deploy mechanism

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gawaksh007.github.io',
-  base: '/gawaksh007.github.io',
+  base: '/',
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
 });
